1b0873ce37808ca1f4fc779bdc400f171de0d3e6
Apple-cake <tongensai.kudasai@gmail.com>
2019-08-08 15:51:38 +0900

    新規作成

---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ChatSpace
+  class Application < Rails::Application
+    config.generators do |g| # controller作成時に不要なファイルが作成されないための記述
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
