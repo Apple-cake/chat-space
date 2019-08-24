b7892896d83d94dabe81feebeddfd2dc51548143
Apple-cake <tongensai.kudasai@gmail.com>
2019-08-24 17:20:12 +0900

    Add some descriptions
    検索結果に一致するユーザー名が非同期で表示される記述を追加
    
    コンソールログの記述を削除

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,33 +1,51 @@
 $(function() {
-  function appenduser(user){
-    var name = user.name ? user.name : '' ;
-        var html = `
-                  <div class="chat-group-user clearfix">
-                    <p class="chat-group-user__name">${user.name}</p>
-                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-                  </div>
-                  `;
-        return html;
+
+  var search_result = $("#user-search-result");
+
+  function appendUser(user){
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+              </div>
+              `;
+    search_result.append(html)
+  }
+  function appendNoUser(user){
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+              </div>
+              `;
+    search_result.append(html)
+  }
+  function addUser(user){
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザー名</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+              </div>
+              `;
+    s.append(html)
   }
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input);
-
     $.ajax({
       type: 'GET',
-      url: '/groups/new',
+      url: '/users',
       data: { keyword: input },
       dataType: 'json'
     })
     .done(function(users) {
-      $("#user-search-field").empty();
+      $("#user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user){
-          appenduser(user);
+          appendUser(user);
         });
       }
       else {
-        appendErrMsgToHTML("一致するユーザーはいません");
+        appendNoUser("一致するユーザーはいません");
       }
     })
     // .fail(function() {
