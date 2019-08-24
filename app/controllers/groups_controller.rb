1d395ccfe84944157ad2ac3eb8124a9684826cb5
Apple-cake <tongensai.kudasai@gmail.com>
2019-08-25 03:33:48 +0900

    Add Incremental search function complete
    新規作成時にログインユーザーがデフォルトで表示され
    グループ編集時に、グループメンバーが表示されるようにしました

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,20 +9,23 @@ class GroupsController < ApplicationController
     @group.users << current_user
   end
 
-  def update
-    if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
-    else
-      render :edit
-    end
-  end
-
   def create
     @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
     end
   end
 
