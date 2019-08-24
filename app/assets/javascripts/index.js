630632e1c656b8f2b0d47ef081a518c56ad31bfe
Apple-cake <tongensai.kudasai@gmail.com>
2019-08-25 00:44:06 +0900

    Add some descriptions
    検索に一致しない場合に正しく表示されるよう修正
    
    メンバーの追加、削除がビューで表示されるようにしました

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,28 +6,29 @@ $(function() {
     var html = `
               <div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
               </div>
               `;
-    search_result.append(html)
+    search_result.append(html);
   }
   function appendNoUser(user){
     var html = `
               <div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${user.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                <p class="chat-group-user__name">${user}</p>
               </div>
               `;
-    search_result.append(html)
+    search_result.append(html);
   }
-  function addUser(user){
+  var user_list = $("#chat-group-user-add");
+  function addUser(user_id,user_name) {
     var html = `
-              <div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">ユーザー名</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+              <div class='chat-group-user'>
+                <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                <p class='chat-group-user__name'>${user_name}</p>
+                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
               </div>
               `;
-    s.append(html)
+    user_list.append(html);
   }
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
@@ -48,8 +49,18 @@ $(function() {
         appendNoUser("一致するユーザーはいません");
       }
     })
-    // .fail(function() {
-    //   alert('失敗しました');
-    // })
+    .fail(function() {
+      alert('失敗しました');
+    })
+  });
+  $(document).on("click",".user-search-add",function() {
+  $('#chat-group-user-add').val();
+    var user_id = $(this).data('user-id');
+    var user_name = $(this).data('user-name');
+    addUser(user_id,user_name);
+    $(this).parent().remove();
+  });
+  $(document).on("click", ".user-search-remove", function () {
+    $(this).parent().remove();
   });
 });
