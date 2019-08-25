80091ae6db442e951fe990f06f943364cb3d42d8
Apple-cake <tongensai.kudasai@gmail.com>
2019-08-25 17:21:22 +0900

    Add some description
    urlの指定を行い
    ５秒おきに自動更新がされるようにした

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -31,7 +31,7 @@ $(function(){
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false,
     })
     .done(function(data){
       var html = buildHTML(data);
@@ -41,30 +41,31 @@ $(function(){
     })
     .fail(function(){
       alert('メッセージエラー');
-    })
+    });
     return false;
-  })
-});
-$(function() {
-  $(function() {
-    var reloadMessages = function() {
-      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-      last_message_id = ※※※
-      $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-        url: ※※※,
-        type: 'get',
-        dataType: 'json',
-        //dataオプションでリクエストに値を含める
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        console.log('success');
-      })
-      .fail(function() {
-        console.log('error');
-      });
-    };
-    setInterval(reloadMessages, 5000);
   });
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    // last_message_id = ※※※
+    $.ajax({
+      url: location.href,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+
+      //メッセージが入ったHTMLを取得
+
+      //メッセージを追加
+
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
