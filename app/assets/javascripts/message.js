cbb8a28abd63006e2105dfd672385f83b649eb81
Apple-cake <tongensai.kudasai@gmail.com>
2019-08-25 21:32:51 +0900

    Add some descriptions
    最新のメッセージを読み込んで
    buildHTMLに追加し画面下にスクロールする記述を追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,27 +45,27 @@ $(function(){
     return false;
   });
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    // last_message_id = ※※※
+    last_message_id = $('.message:last').data('message-id');
     $.ajax({
       url: location.href,
       type: 'get',
       dataType: 'json',
       data: {id: last_message_id}
     })
-    .done(function(messages) {
-      //追加するHTMLの入れ物を作る
+    .done(function(data) {
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-
-      //メッセージが入ったHTMLを取得
-
-      //メッセージを追加
+      data.forEach()
+        if (data.length) {
+          insertHTML += buildHTML(data);
+        }
+      $('.messages').append(insertHTML)
+      $('.messages').delay(100).animate({scrollTop: $('.messages')[0].scrollHeight}, 'swing');
 
     })
     .fail(function() {
-      console.log('error');
+      alert('自動更新に失敗しました')
     });
   };
-  setInterval(reloadMessages, 5000);
+  console.log(last_message_id);
+  // setInterval(reloadMessages, 1000000);
 });
