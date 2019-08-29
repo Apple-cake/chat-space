a51a4236e33e20506634902dbd8f03062bd0751f
Apple-cake <tongensai.kudasai@gmail.com>
2019-08-29 13:29:15 +0900

    修正：タイムゾーン、二重サブミット
    自動更新時の日時表記を修正
    自動更新でも日本時間で表示されるよう修正しました。
    
    二重サブミット対策の記述を追加しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -25,26 +25,27 @@ $(document).on('turbolinks:load',(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false,
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html)
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
-    })
-    .fail(function(){
-      alert('メッセージエラー');
-    })
-    .always(function () {
-      $(".form__submit").removeAttr("disabled");
-      $('form')[0].reset()
-    })
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+        $('.box__message').val("");
+        $('.hidden').val("");
+      })
+      .fail(function(){
+        alert('メッセージエラー');
+      })
+      .always(function () {
+        $(".form__submit").removeAttr("disabled");
+      })
   });
   var reloadMessages = function(){
     var last_message_id = $('.message:last').data("id");
